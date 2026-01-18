a44b239732a3ea630bdc6b2a155fc209a694b268
Renato Shimabukuro <renato.shimabukuro.2@gmail.com>
2026-01-18 14:49:57 +0900

    CRUD done

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,36 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params_list)
+    if @list.save
+      redirect_to @list
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+
+    redirect_to lists_path status: :see_other
+  end
+
+private
+
+def params_list
+  params.require(:list).permit(:name)
+end
+
+end
